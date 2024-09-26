c3e479a26f8d0fec359f01174047a7de23dd7484
Rinkit Adhana <therinkit@gmail.com>
2024-09-26 14:13:29 +0530

    add

--- api/index.js
+++ api/index.js
@@ -1,16 +1,13 @@
-import express from "express"
-import Data from "./data.js"
+import express from "express";
+import Data from "./data.js";
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
 app.get("/api/data", (req, res) => {
-  res.send(Data)
-})
-app.get("/", (req, res) => {
-  res.send("Hello")
-})
+  res.send(Data);
+});
 
 app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}/api/data`)
-})
+  console.log(`Listening on http://localhost:${PORT}/api/data`);
+});
