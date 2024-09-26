9e146befd4ec881d58b36d3bc9c4031b2d0c0b74
Rinkit Adhana <therinkit@gmail.com>
2024-09-26 14:14:40 +0530

    add

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://therinkit-tweetspree.vercel.app/",
+      "/api": "https://tweet-spree-api.vercel.app",
     },
   },
   plugins: [react()],
